0de44d78c79a02a27b7c530401de1c4f12dde1ae
RamaSandhy <rshandy07@gmail.com>
2023-06-30 14:17:43 +0700

    menambahkan constructor untuk sebuah Node class dengan info, leftchild, dan rightchild parameters

--- BinarySearchTree_029/BinarySearchTree_029.cpp
+++ BinarySearchTree_029/BinarySearchTree_029.cpp
@@ -7,5 +7,13 @@ class Node
 public:
 	string info;
 	Node* leftchild;
-	Node* roghtchild;
+	Node* rightchild;
+
+	// Constructor for the node class
+	Node(string i, Node* l, Node* r)
+	{
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
 };
